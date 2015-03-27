2351f9ac3912dfb0c44f1f4b60d91a59ef5bb34b
Andrew Zhao <snareclaw@gmail.com>
2015-03-26 23:27:34 -0700

    Add Javascript and more HTML/CSS
    Javascript app written and included in app/assets/javascripts. Added a Error div to display errors and such. The javascript is currently  working for the most part, however the click event seems to be firing twice for some reason, resulting in the already selected error message popping up every time a click occurs. Also the Game Ended error message isn't prevent clicks from happening.

---
+++ app/assets/javascripts/tictactoe.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  // Initialize the player symbols, the index of the first player as the current player, a check for the game being over, and the number of remaining tiles..
+  var players = ['X', 'O'];
+  var current_player = 0;
+  var game_over = false;
+  var remaining_tiles = 9;
+
+  // Initialize the board. It is represented by an length 3 array of length 3 arrays.
+  // Values: -1 is no player mark, 0 is player 0's mark, 1 is player 1's mark.
+  var board = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]];
+  
+  // Define a function that compares 3 cells, and returns true if they're all the same player and false otherwise.
+  function compare(x, y, z) {
+    return (x == y) && (x == z) && (x > -1)
+  }
+  
+  // Define a function that checks the board for a win. Outputs true if a winner is found, false otherwise.
+  function winner() {
+    if (compare(board[0][0], board[1][1], board[2][2]) || compare(board[0][1], board[1][1], board[2][1]) ||
+        compare(board[0][2], board[1][1], board[2][0]) || compare(board[1][0], board[1][1], board[1][2]) ||
+        compare(board[0][0], board[0][1], board[0][2]) || compare(board[0][0], board[1][0], board[2][0]) ||
+        compare(board[0][2], board[1][2], board[2][2]) || compare(board[2][0], board[2][1], board[2][2])) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+    
+
+  // Define the action that occurs if a cell is clicked on.
+  $('#board td').click(function(event) {
+    // Check for errors and output a message if found.
+    if (game_over == true) {
+      document.getElementById('error').innerHTML = "ERROR: Game already ended. No move cannot be made.";
+    }
+    else if ($(this).text()!= '-') {
+      document.getElementById("error").innerHTML = "ERROR: That spot has already been selected!";
+    }
+    else {
+      // Delete any error messages left from previous actions.
+      if (document.getElementById('error').innerHTML != "") {
+        document.getElementById('error').innerHTML = "";
+      }
+      // Set the current tile to the current player's marker.
+      this.innerHTML = players[current_player];
+      var coord = $(this).attr("id");
+      board[coord[0]][coord[1]] = current_player;
+      remaining_tiles -= 1;
+      // Check for game ending conditions
+      if (winner()) {
+        document.getElementById('victory').innerHTML = "Winner: Player " + players[current_player];
+        game_over = true
+      }
+      else if (remaining_tiles == 0) {
+        document.getElementById('victory').innerHTML = "Draw";
+        game_over = true;
+      }
+      else {
+        // If no win, switch players.
+        current_player = (current_player + 1) % 2;
+        document.getElementById('player-id').innerHTML = players[current_player];
+      }
+    }
+  })
+})
